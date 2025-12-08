260902c85a5a6b587afa0b89c4b3ce468403ed27
Igor Vaiman <igor.vaiman@inten.to>
2025-12-08 20:32:33 +0100

    d8 pt2 solution

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,9 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet, VecDeque},
-    hash::Hash,
-};
-
-use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
 
 use crate::{solution::Solution, visualizer::Visualizer};
 
@@ -27,21 +22,10 @@ impl Solution for Playground {
     }
 
     fn solve_pt1(&self, input: Self::InputT, _visualizer: &mut dyn Visualizer) -> Self::OutputT {
-        let mut connections: Vec<(usize, usize, f64)> = Vec::new();
-        for (i, a) in input.iter().enumerate() {
-            for j in (i + 1)..input.len() {
-                let b = input[j];
-                let distance =
-                    (((a[0] - b[0]).pow(2) + (a[1] - b[1]).pow(2) + (a[2] - b[2]).pow(2)) as f64)
-                        .sqrt();
-                connections.push((i, j, distance))
-            }
-        }
-        connections.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap());
+        let connections = make_connections(&input);
 
         let mut conn_list: HashMap<usize, HashSet<usize>> = HashMap::new();
         for (i, j, _) in connections.iter().take(1000) {
-            println!("connecting {} - {}", i, j);
             if !conn_list.contains_key(i) {
                 conn_list.insert(*i, HashSet::new());
             }
@@ -52,8 +36,6 @@ impl Solution for Playground {
             }
             conn_list.get_mut(j).unwrap().insert(*i);
         }
-
-        println!("{:?}", conn_list);
 
         let mut visited: HashSet<usize> = HashSet::new();
         let mut circuit_sizes = Vec::<usize>::new();
@@ -87,12 +69,62 @@ impl Solution for Playground {
         }
 
         circuit_sizes.sort_by(|a, b| b.cmp(a));
-        println!("{:?}", circuit_sizes);
 
-        circuit_sizes.into_iter().take(3).reduce(|acc, v| acc * v).unwrap()
+        circuit_sizes
+            .into_iter()
+            .take(3)
+            .reduce(|acc, v| acc * v)
+            .unwrap()
     }
 
     fn solve_pt2(&self, input: Self::InputT, _visualizer: &mut dyn Visualizer) -> Self::OutputT {
+        let mut circuit_members: HashMap<usize, HashSet<usize>> = (0..input.len())
+            .map(|jid| {
+                let mut s = HashSet::new();
+                s.insert(jid);
+                (jid, s)
+            })
+            .collect();
+        let mut next_circuit_id: usize = input.len();
+        let mut circuit: HashMap<usize, usize> = (0..input.len()).map(|jid| (jid, jid)).collect();
+        for (i, j, _) in make_connections(&input) {
+            let circuit_id_i = circuit.get(&i).unwrap();
+            let circuit_id_j = circuit.get(&j).unwrap();
+            if circuit_id_i == circuit_id_j {
+                continue; // no effect on the circuits
+            }
+
+            let mut new_circuit_members = HashSet::<usize>::new();
+            new_circuit_members.extend(circuit_members.remove(circuit_id_i).unwrap());
+            new_circuit_members.extend(circuit_members.remove(circuit_id_j).unwrap());
+
+            for id in new_circuit_members.iter() {
+                circuit.insert(*id, next_circuit_id); // reassign all members to the newly created joint circuit
+            }
+            circuit_members.insert(next_circuit_id, new_circuit_members);
+            next_circuit_id += 1;
+
+            if next_circuit_id > 2 && circuit_members.len() == 1 {
+                println!("{:?}", input[i]);
+                println!("{:?}", input[j]);
+                return (input[i][0] * input[j][0]) as usize;
+            }
+        }
         0
     }
+}
+
+fn make_connections(input: &Vec<[i64; 3]>) -> Vec<(usize, usize, f64)> {
+    let mut connections: Vec<(usize, usize, f64)> = Vec::new();
+    for (i, a) in input.iter().enumerate() {
+        for j in (i + 1)..input.len() {
+            let b = input[j];
+            let distance = (((a[0] - b[0]).pow(2) + (a[1] - b[1]).pow(2) + (a[2] - b[2]).pow(2))
+                as f64)
+                .sqrt();
+            connections.push((i, j, distance))
+        }
+    }
+    connections.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap());
+    connections
 }
